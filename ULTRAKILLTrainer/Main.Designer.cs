54a825014729ae20542b02119321a03faebd43d1
Saniee <asamsku10@gmail.com>
2022-07-08 18:41:37 +0200

    Update Main.Designer.cs

--- ULTRAKILLTrainer/Main.Designer.cs
+++ ULTRAKILLTrainer/Main.Designer.cs
@@ -51,16 +51,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(118, 157);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Game Open:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             // 
             // ProcOpenLabel
             // 
             this.ProcOpenLabel.AutoSize = true;
-            this.ProcOpenLabel.Location = new System.Drawing.Point(114, 9);
+            this.ProcOpenLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProcOpenLabel.Location = new System.Drawing.Point(118, 178);
             this.ProcOpenLabel.Name = "ProcOpenLabel";
             this.ProcOpenLabel.Size = new System.Drawing.Size(38, 21);
             this.ProcOpenLabel.TabIndex = 1;
@@ -69,7 +72,7 @@
             // InfHealth
             // 
             this.InfHealth.AutoSize = true;
-            this.InfHealth.Location = new System.Drawing.Point(12, 33);
+            this.InfHealth.Location = new System.Drawing.Point(12, 12);
             this.InfHealth.Name = "InfHealth";
             this.InfHealth.Size = new System.Drawing.Size(126, 25);
             this.InfHealth.TabIndex = 2;
@@ -80,7 +83,7 @@
             // InfDash
             // 
             this.InfDash.AutoSize = true;
-            this.InfDash.Location = new System.Drawing.Point(12, 64);
+            this.InfDash.Location = new System.Drawing.Point(12, 43);
             this.InfDash.Name = "InfDash";
             this.InfDash.Size = new System.Drawing.Size(116, 25);
             this.InfDash.TabIndex = 3;
@@ -91,7 +94,7 @@
             // InfRailgun
             // 
             this.InfRailgun.AutoSize = true;
-            this.InfRailgun.Location = new System.Drawing.Point(12, 95);
+            this.InfRailgun.Location = new System.Drawing.Point(12, 74);
             this.InfRailgun.Name = "InfRailgun";
             this.InfRailgun.Size = new System.Drawing.Size(134, 25);
             this.InfRailgun.TabIndex = 4;
@@ -102,7 +105,7 @@
             // InfNailgun
             // 
             this.InfNailgun.AutoSize = true;
-            this.InfNailgun.Location = new System.Drawing.Point(12, 126);
+            this.InfNailgun.Location = new System.Drawing.Point(12, 105);
             this.InfNailgun.Name = "InfNailgun";
             this.InfNailgun.Size = new System.Drawing.Size(187, 25);
             this.InfNailgun.TabIndex = 5;
@@ -133,7 +136,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 266);
+            this.ClientSize = new System.Drawing.Size(239, 266);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.InfNailgun);
