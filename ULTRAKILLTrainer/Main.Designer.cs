e0b211247e8b8ce50448c1b05350c399e0913bde
Saniee <asamsku10@gmail.com>
2022-07-19 01:29:45 +0200

    Update From Debug Version
    - Added Error Handling
    - Added One Shot Enemies
    - Infinite Jump
    - Infinite Revolver/Pistol Coins
    - Walk Speed Slider
    - Jump Force Slider

--- ULTRAKILLTrainer/Main.Designer.cs
+++ ULTRAKILLTrainer/Main.Designer.cs
@@ -38,7 +38,16 @@
             this.InfNailgun = new System.Windows.Forms.CheckBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.OneShot = new System.Windows.Forms.CheckBox();
+            this.InfJump = new System.Windows.Forms.CheckBox();
+            this.walkSpeedBar = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.jumpForceBar = new System.Windows.Forms.TrackBar();
+            this.InfCoins = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.walkSpeedBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jumpForceBar)).BeginInit();
             this.SuspendLayout();
             // 
             // BGWorker
@@ -51,16 +60,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(118, 209);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Game Open:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             // 
             // ProcOpenLabel
             // 
             this.ProcOpenLabel.AutoSize = true;
-            this.ProcOpenLabel.Location = new System.Drawing.Point(114, 9);
+            this.ProcOpenLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProcOpenLabel.Location = new System.Drawing.Point(118, 230);
             this.ProcOpenLabel.Name = "ProcOpenLabel";
             this.ProcOpenLabel.Size = new System.Drawing.Size(38, 21);
             this.ProcOpenLabel.TabIndex = 1;
@@ -69,7 +81,7 @@
             // InfHealth
             // 
             this.InfHealth.AutoSize = true;
-            this.InfHealth.Location = new System.Drawing.Point(12, 33);
+            this.InfHealth.Location = new System.Drawing.Point(12, 12);
             this.InfHealth.Name = "InfHealth";
             this.InfHealth.Size = new System.Drawing.Size(126, 25);
             this.InfHealth.TabIndex = 2;
@@ -80,7 +92,7 @@
             // InfDash
             // 
             this.InfDash.AutoSize = true;
-            this.InfDash.Location = new System.Drawing.Point(12, 64);
+            this.InfDash.Location = new System.Drawing.Point(12, 43);
             this.InfDash.Name = "InfDash";
             this.InfDash.Size = new System.Drawing.Size(116, 25);
             this.InfDash.TabIndex = 3;
@@ -91,7 +103,7 @@
             // InfRailgun
             // 
             this.InfRailgun.AutoSize = true;
-            this.InfRailgun.Location = new System.Drawing.Point(12, 95);
+            this.InfRailgun.Location = new System.Drawing.Point(12, 74);
             this.InfRailgun.Name = "InfRailgun";
             this.InfRailgun.Size = new System.Drawing.Size(134, 25);
             this.InfRailgun.TabIndex = 4;
@@ -102,7 +114,7 @@
             // InfNailgun
             // 
             this.InfNailgun.AutoSize = true;
-            this.InfNailgun.Location = new System.Drawing.Point(12, 126);
+            this.InfNailgun.Location = new System.Drawing.Point(12, 105);
             this.InfNailgun.Name = "InfNailgun";
             this.InfNailgun.Size = new System.Drawing.Size(187, 25);
             this.InfNailgun.TabIndex = 5;
@@ -113,7 +125,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ULTRAKILLTrainer.Properties.Resources.tenebris_vector_art;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 157);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 209);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -123,17 +135,99 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(118, 215);
+            this.label2.Location = new System.Drawing.Point(118, 267);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(101, 42);
             this.label2.TabIndex = 7;
             this.label2.Text = "Made By\r\nSaniee#0007";
             // 
+            // OneShot
+            // 
+            this.OneShot.AutoSize = true;
+            this.OneShot.Location = new System.Drawing.Point(12, 136);
+            this.OneShot.Name = "OneShot";
+            this.OneShot.Size = new System.Drawing.Size(156, 25);
+            this.OneShot.TabIndex = 8;
+            this.OneShot.Text = "One Shot Enemies";
+            this.OneShot.UseVisualStyleBackColor = true;
+            this.OneShot.CheckedChanged += new System.EventHandler(this.OneShot_CheckedChanged);
+            // 
+            // InfJump
+            // 
+            this.InfJump.AutoSize = true;
+            this.InfJump.Location = new System.Drawing.Point(12, 167);
+            this.InfJump.Name = "InfJump";
+            this.InfJump.Size = new System.Drawing.Size(119, 25);
+            this.InfJump.TabIndex = 9;
+            this.InfJump.Text = "Infinite Jump";
+            this.InfJump.UseVisualStyleBackColor = true;
+            this.InfJump.CheckedChanged += new System.EventHandler(this.InfJump_CheckedChanged);
+            // 
+            // walkSpeedBar
+            // 
+            this.walkSpeedBar.LargeChange = 250;
+            this.walkSpeedBar.Location = new System.Drawing.Point(242, 185);
+            this.walkSpeedBar.Maximum = 1500;
+            this.walkSpeedBar.Minimum = 750;
+            this.walkSpeedBar.Name = "walkSpeedBar";
+            this.walkSpeedBar.Size = new System.Drawing.Size(178, 45);
+            this.walkSpeedBar.SmallChange = 100;
+            this.walkSpeedBar.TabIndex = 10;
+            this.walkSpeedBar.Value = 750;
+            this.walkSpeedBar.ValueChanged += new System.EventHandler(this.walkSpeedBar_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 21);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Walk Speed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 21);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Jump Force";
+            // 
+            // jumpForceBar
+            // 
+            this.jumpForceBar.Location = new System.Drawing.Point(242, 267);
+            this.jumpForceBar.Maximum = 180;
+            this.jumpForceBar.Minimum = 90;
+            this.jumpForceBar.Name = "jumpForceBar";
+            this.jumpForceBar.Size = new System.Drawing.Size(178, 45);
+            this.jumpForceBar.TabIndex = 13;
+            this.jumpForceBar.Value = 90;
+            this.jumpForceBar.ValueChanged += new System.EventHandler(this.jumpForceBar_ValueChanged);
+            // 
+            // InfCoins
+            // 
+            this.InfCoins.AutoSize = true;
+            this.InfCoins.Location = new System.Drawing.Point(230, 12);
+            this.InfCoins.Name = "InfCoins";
+            this.InfCoins.Size = new System.Drawing.Size(120, 25);
+            this.InfCoins.TabIndex = 14;
+            this.InfCoins.Text = "Infinite Coins";
+            this.InfCoins.UseVisualStyleBackColor = true;
+            this.InfCoins.CheckedChanged += new System.EventHandler(this.InfCoins_CheckedChanged);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 266);
+            this.ClientSize = new System.Drawing.Size(432, 321);
+            this.Controls.Add(this.InfCoins);
+            this.Controls.Add(this.jumpForceBar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.walkSpeedBar);
+            this.Controls.Add(this.InfJump);
+            this.Controls.Add(this.OneShot);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.InfNailgun);
@@ -146,10 +240,14 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MdiChildrenMinimizedAnchorBottom = false;
             this.Name = "Main";
             this.Text = "ULTRAKILL Trainer";
             this.Shown += new System.EventHandler(this.Main_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.walkSpeedBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jumpForceBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,5 +264,12 @@
         private CheckBox InfNailgun;
         private PictureBox pictureBox1;
         private Label label2;
+        private CheckBox OneShot;
+        private CheckBox InfJump;
+        private TrackBar walkSpeedBar;
+        private Label label3;
+        private Label label4;
+        private TrackBar jumpForceBar;
+        private CheckBox InfCoins;
     }
 }
--- ULTRAKILLTrainer/Main.cs
+++ ULTRAKILLTrainer/Main.cs
@@ -1,18 +1,46 @@
 using Memory;
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace ULTRAKILLTrainer
 {
     public partial class Main : Form
     {
         Mem m = new Mem();
+        private string errorCode = "0";
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool VirtualFreeEx(IntPtr hProcess, UIntPtr lpAddress, UIntPtr dwSize, uint dwFreeType);
+
+        public bool DeAllocRegion(UIntPtr codecavebase, IntPtr openedHandle)
+        {
+            // maybe check if codecavebase is uintptr.zero and openedhandle is intptr.zero etc
+
+            return VirtualFreeEx(openedHandle, codecavebase, (UIntPtr)0, 0x00008000);
+        }
 
         public Main()
         {
             InitializeComponent();
         }
 
-        bool ProcOpen = false;
+        private Form1 errorForm;
 
+        bool ProcOpen = false;
+        private UIntPtr codeCaveDamage;
+        long aobDamageScan;
+
+        string playerBase = "mono-2.0-bdwgc.dll+00494A90,DE8,0,F8,20";
+        string wepCharges = "mono-2.0-bdwgc.dll+00494A90,E50,28,A0,250,20";
+        string coinRecharge = ",38";
+        string walkSpeed = ",1A0";
+        string jumpForce = ",1A4";
+        string playerHealth = ",20C";
+        string nailgunAmmo = ",4C";
+        string railgunCharge = ",5C";
+        string boostRecharge = ",288";
+        string fallingBool = ",1B1";
 
         private void Main_Shown(object sender, EventArgs e)
         {
@@ -51,11 +79,11 @@ namespace ULTRAKILLTrainer
         {
             if (InfHealth.Checked)
             {
-                m.FreezeValue("mono-2.0-bdwgc.dll+00494A90,DE8,0,F8,20,20C", "int", "1000");
+                m.FreezeValue(playerBase + playerHealth, "int", "1000");
             }
             else
             {
-                m.UnfreezeValue("mono-2.0-bdwgc.dll+00494A90,DE8,0,F8,20,20C");
+                m.UnfreezeValue(playerBase + playerHealth);
             }
         }
 
@@ -63,11 +91,11 @@ namespace ULTRAKILLTrainer
         {
             if (InfDash.Checked)
             {
-                m.FreezeValue("mono-2.0-bdwgc.dll+00494A90,DE8,0,F8,20,288", "float", "300");
+                m.FreezeValue(playerBase + boostRecharge, "float", "300");
             }
             else
             {
-                m.UnfreezeValue("mono-2.0-bdwgc.dll+00494A90,DE8,0,F8,20,288");
+                m.UnfreezeValue(playerBase + boostRecharge);
             }
         }
 
@@ -75,11 +103,11 @@ namespace ULTRAKILLTrainer
         {
             if (InfRailgun.Checked)
             {
-                m.FreezeValue("UnityPlayer.dll+01815FA0,A90,180,D0,48,F0,0,5C", "float", "5");
+                m.FreezeValue(wepCharges + railgunCharge, "float", "5");
             }
             else
             {
-                m.UnfreezeValue("UnityPlayer.dll+01815FA0,A90,180,D0,48,F0,0,5C");
+                m.UnfreezeValue(wepCharges + railgunCharge);
             }
         }
 
@@ -87,11 +115,81 @@ namespace ULTRAKILLTrainer
         {
             if (InfNailgun.Checked)
             {
-                m.FreezeValue("UnityPlayer.dll+01815FA0,A90,180,D0,48,F0,0,4C", "float", "100");
+                m.FreezeValue(wepCharges + nailgunAmmo, "float", "100");
             }
             else
             {
-                m.UnfreezeValue("UnityPlayer.dll+01815FA0,A90,180,D0,48,F0,0,4C");
+                m.UnfreezeValue(wepCharges + nailgunAmmo);
+            }
+        }
+
+        private async void OneShot_CheckedChanged(object sender, EventArgs e)
+        {
+            errorCode = "404 PNTR";
+            if (OneShot.Checked)
+            {
+                byte[] newBytes =
+                {
+                    0xC7, 0x45, 0x30, 0x00, 0x00, 0x7A, 0x44
+                };
+                aobDamageScan = (await m.AoBScan("F3 0F 11 6D 30 49 8B 47", false, true)).FirstOrDefault();
+                if (aobDamageScan == 0)
+                {
+                    Debug.WriteLine("Scan Unsuccsesful.");
+                    errorForm = new Form1();
+                    errorForm.Show(errorCode);
+                } 
+                else
+                {
+                    Debug.WriteLine("Scan Succses! Addres found: " + aobDamageScan.ToString("X"));
+                    codeCaveDamage = m.CreateCodeCave(aobDamageScan.ToString("X"), newBytes, 5, 1000);
+                }
+            }
+            else
+            {
+                DeAllocRegion(codeCaveDamage, m.mProc.Handle);
+                Debug.WriteLine("Deallocated Region " + aobDamageScan.ToString("X"));
+                m.WriteMemory(aobDamageScan.ToString("X"), "bytes", "F3 0F 11 6D 30");
+                Debug.WriteLine("Wrote original assembly into the scan address.");
+            }
+        }
+
+        private void InfJump_CheckedChanged(object sender, EventArgs e)
+        {
+            if (InfJump.Checked)
+            {
+                m.FreezeValue(playerBase + fallingBool, "int", "0");
+            }
+            else
+            {
+                m.UnfreezeValue(playerBase + fallingBool);
+            }
+        }
+
+        private void walkSpeedBar_Scroll(object sender, EventArgs e)
+        {
+            m.WriteMemory(playerBase + walkSpeed, "float", walkSpeedBar.Value.ToString());
+        }
+
+        private void walkSpeedBar_ValueChanged(object sender, EventArgs e)
+        {
+            m.WriteMemory(playerBase + walkSpeed, "float", walkSpeedBar.Value.ToString());
+        }
+
+        private void jumpForceBar_ValueChanged(object sender, EventArgs e)
+        {
+            m.WriteMemory(playerBase + jumpForce, "float", jumpForceBar.Value.ToString());
+        }
+
+        private void InfCoins_CheckedChanged(object sender, EventArgs e)
+        {
+            if (InfCoins.Checked)
+            {
+                m.FreezeValue(wepCharges + coinRecharge, "float", "400");
+            }
+            else
+            {
+                m.UnfreezeValue(wepCharges + coinRecharge);
             }
         }
     }
