b1c9870c1e8f26983284a27ba999bf87a8911869
Saniee <asamsku10@gmail.com>
2023-04-19 17:44:39 +0200

    Fresh Code, New Project File

---
+++ MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace ULTRAKILLTrainer
+{
+    public partial class MainApp : Form
+    {
+        public Mem m = new Mem();
+
+        public MainApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
